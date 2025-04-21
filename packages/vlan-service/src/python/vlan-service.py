ea2191beb3f4c4f68796de229e7d4ddc4870ad68
msiripella <manikantasiripella@siripella.local>
2025-04-21 22:31:33 +0530

    nso-vlan-service

---
+++ packages/vlan-service/src/python/vlan-service.py
@@ -0,0 +1,10 @@
+from ncs.application import Service
+
+class Main(Service):
+    def create(self, tctx, root, service, proplist):
+        self.log.info(f"Creating VLAN {service.vlan_id} on device {service.device}")
+        vlan_template = self.ncs.template.Template(service)
+        vlan_template.apply('vlan-template', {
+            'VLAN_ID': service.vlan_id,
+            'DEVICE_NAME': service.device
+        })
